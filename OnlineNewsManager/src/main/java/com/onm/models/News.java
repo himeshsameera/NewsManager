d412d0ad8bda5083fafdac59e69e36274e032f3a
Himesh <himeshsameera@gmail.com>
2013-09-23 13:59:15 +0530

    The initial project with spring framework.
    Here a sample with spring framework working is added.

---
+++ OnlineNewsManager/src/main/java/com/onm/models/News.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.onm.models;
+
+/**
+ *
+ * @author Himesh_2
+ */
+public class News {
+    
+}
